727f5341144d3b018dc655cab935f3c1f80246df
marwaelkelani <mar.elkelani@gmail.com>
2023-08-02 17:41:37 -0400

    create api route to find product by id in param url

--- server-side/index.js
+++ server-side/index.js
@@ -95,12 +95,20 @@ app.get('/products', (req,res)=>{
 })
 
 
-//Filter products
+//Filter products by product id
+app.get('/products/:id', (req,res)=>{
+    let id = parseInt(req.params.id);
 
-
-//Find a product by link or in the url
-
-
+    Product.findByPk(id).then((result)=>{
+        if(result){
+            res.status(200).send(result);
+        }else {
+            res.status(404).send('Product not found');
+        }
+    }).catch((err)=>{
+        res.status(500).send(err);
+    });
+});
 
 
 //Web Server
