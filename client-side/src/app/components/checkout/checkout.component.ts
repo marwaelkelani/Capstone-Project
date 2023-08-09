63d7d9ee4723faff826c06fe43d63b305c7d3fe0
marwaelkelani <mar.elkelani@gmail.com>
2023-08-09 14:22:54 -0400

    Completed checkout form validation

--- client-side/src/app/components/checkout/checkout.component.ts
+++ client-side/src/app/components/checkout/checkout.component.ts
@@ -8,17 +8,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CheckoutComponent {
 checkoutForm!: FormGroup
+redColor = 'red';
 
   constructor(private fb: FormBuilder){
     this.checkoutForm = fb.group({
       first_name: ['', [Validators.required]],
       last_name: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      card_number: ['', Validators.required], //fix validations
+      card_number: ['', [Validators.required, Validators.minLength(16)]], //fix validations
       card_name: ['', Validators.required],
       expiry_date: ['', Validators.required],
       security_number: ['', [Validators.required, Validators.minLength(3)]]
     })
+  }
+
+  onPayment(){
+    alert('Thank you! Your payment has been processed!');
+    console.log(this.checkoutForm.value);
   }
 
   //Submit Function goes here
@@ -29,5 +35,28 @@ checkoutForm!: FormGroup
     return this.checkoutForm.get('first_name')!;
   }
 
+  get last_name(){
+    return this.checkoutForm.get('last_name')!;
+  }
+
+  get email(){
+    return this.checkoutForm.get('email')!;
+  }
+
+  get card_number(){
+    return this.checkoutForm.get('card_number')!;
+  }
+
+  get card_name(){
+    return this.checkoutForm.get('card_name')!;
+  }
+
+  get expiry_date(){
+    return this.checkoutForm.get('expiry_date')!;
+  }
+
+  get security_number(){
+    return this.checkoutForm.get('security_number')!;
+  }
 
 }
